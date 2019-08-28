cc230bb5b7b07d825bfff24025e17f5617a03b05
Thomas DiGregorio <digregoriothomas@yahoo.com>
2019-08-28 13:57:26 -0700

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class MusicLibraryController
     def initialize(path = "./db/mp3s")
         MusicImporter.new(path).import
@@ -41,8 +42,15 @@ class MusicLibraryController
         def list_songs_by_artist
             puts "Please enter the name of an artist:"
             input = gets.strip
-            puts 
+            artist = Artist.find_by_name(input)
+            if artist
+             #"error message" 
+            #else 
+            sorted = artist.songs.sort  { |a, b| a.name <=> b.name }.each_with_index do |song, idx|
+                puts "#{idx + 1}. #{song.artist.name} - #{song.genre.name}"
+            end
         end
+    end           
     end 
     
     
